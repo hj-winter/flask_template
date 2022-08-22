6d06c661093a1b98732b696fdea6e2ebc1e75249
Hermann-Josef Winter <hjw2803@googlemail.com>
2022-08-22 15:18:35 +0200

    basic configuration with bootstrap added

--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ASqVjo9xzF4sF7gG7EpJAhPQ1zBTIS'
 
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
-
-
